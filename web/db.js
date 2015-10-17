a039db1c64aba6eb79ea5a176f07711b55203781
SEIAROTg <seiarotg@gmail.com>
2015-10-17 18:15:49 +0800

    add db.js

---
+++ web/db.js
@@ -0,0 +1,19 @@
+var path       = require('path');
+var level      = require('level');
+var sqlite3    = require('sqlite3');
+
+var config     = require('../config');
+
+function dbGetAbsPath(relPath) {
+	return path.resolve(__dirname, '..', 'db', relPath);
+}
+
+var userbind      = level(dbGetAbsPath(config.db.level.userbind));
+var y1timetable   = new sqlite3.Database(dbGetAbsPath(config.db.sqlite.y1timetable));
+var coal          = new sqlite3.Database(dbGetAbsPath(config.db.sqlite.coal));
+
+module.exports = {
+	userbind      : userbind,
+	y1timetable   : y1timetable,
+	coal          : coal
+}
