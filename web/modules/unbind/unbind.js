4f0665b13b00deba9929521849f54fed4e5959a7
SEIAROTg <seiarotg@gmail.com>
2015-10-17 19:30:41 +0800

    add unbind

---
+++ web/modules/unbind/unbind.js
@@ -0,0 +1,16 @@
+var common       = require('../common');
+
+var responses = require('../wechatInterface/responses');
+
+function unbind (req, res) {
+	common.getUser(req, res, function (data) {
+		data.stuId = null;
+		data.build = null;
+		data.room = null;
+		common.putUser(req, res, data, function () {
+			responses.sendResponse(res, 'success', null, req);
+		});
+	});
+}
+
+module.exports = unbind;
