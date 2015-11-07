b2bf63b36866b217453358280b7e6cf9c30b1083
SEIAROTg <seiarotg@gmail.com>
2015-11-08 00:39:08 +0800

    change template engine for wechat responses into ejs

--- web/modules/wechatInterface/responses.js
+++ web/modules/wechatInterface/responses.js
@@ -1,21 +1,7 @@
-var path    = require('path');
-var fs      = require('fs');
-
-var filelist = fs.readdirSync(path.resolve(__dirname, 'templates'));
-
-var templates = {};
-
-for (var idx in filelist) {
-	var match = filelist[idx].match(/^(.*)\.xml$/);
-	if (match) {
-		templates[match[1]] = fs.readFileSync(path.resolve(__dirname, 'templates', match[0]), 'utf-8');
-	}
-}
-
-function getResponse(templateName, data, req) {
+function sendResponse(res, templateName, data, req) {
 
 	if (!data) {
-		data = {}
+		data = {};
 	}
 
 	if (req) {
@@ -23,25 +9,9 @@ function getResponse(templateName, data, req) {
 		data.fromUser = req.tousername[0];
 		data.timestamp = Date.now() / 1000 | 0;
 	}
-
-	var response = templates[templateName];
-	response = response.replace(/\{\{(?:(\{\{)|(.*?)\}\})/g, function (p0, p1, p2) {
-		if (p1) {
-			return '{{';
-		} else if (p2 && p2 in data) {
-			return data[p2];
-		} else {
-			return p0;
-		}
-	}).replace(/\}\}\}\}/g, '}}');
-	return response;
-}
-
-function sendResponse(res, templateName, data, req) {
-	res.type('xml').end(getResponse(templateName, data, req));
+	res.type('xml').render('wechatResponses/' + templateName, data);
 }
 
 module.exports = {
-	getResponse: getResponse,
 	sendResponse: sendResponse
 };
