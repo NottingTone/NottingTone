4258eb172aadf334823f60c1335256906848e451
SEIAROTg <seiarotg@gmail.com>
2015-11-09 23:55:16 +0800

    change wechat interface

--- web/routes.js
+++ web/routes.js
@@ -1,6 +1,6 @@
 var router = require('express').Router();
 
-var wechatInterface = require('./modules/wechatInterface/wechatInterface.js');
+var wechatInterface = require('./wechatInterface.js');
 router.use('/wechat/interface', wechatInterface);
 
 module.exports = router;
---
+++ web/wechatInterface.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var xmlparser    = require('express-xml-bodyparser');
+var crypto       = require('crypto');
+var config       = require('../config');
+var handler      = require('./handler');
+
+var router = require('express').Router();
+
+function checkSigniture (timestamp, nonce, signature) {
+	var t = [config.wechat.token, timestamp, nonce];
+	t = t.sort().join('');
+	t = crypto.createHash('sha1').update(t).digest('hex');
+	return t === signature;
+}
+
+// 检查token
+router.use(function(req, res, next) {
+	if (!checkSigniture(req.query.timestamp, req.query.nonce, req.query.signature)) {
+		res.status(404).end();
+	} else {
+		next();
+	}
+});
+
+router.use(xmlparser());
+
+// 微信接口地址配置
+router.get('/', function (req, res) {
+	res.status(200).end(req.query.echostr);
+});
+
+// 相应微信事件
+router.post('/', function (req, res) {
+
+	function getValue (key) {
+		return req.body[key] && req.body[key][0];
+	}
+
+	var wxEvent = {
+		openid        : getValue('fromusername'),
+		type          : getValue('msgtype'),
+		content       : getValue('content'),
+		picUrl        : getValue('picurl'),
+		mediaId       : getValue('mediaid'),
+		format        : getValue('format'),
+		recognition   : getValue('recognition'),
+		thumbMediaId  : getValue('thumbmediaid'),
+		locationX     : getValue('location_x'),
+		locationY     : getValue('location_y'),
+		scale         : getValue('scale'),
+		title         : getValue('title'),
+		description   : getValue('description'),
+		url           : getValue('url')
+	}
+
+	handler.initializeHandler(wxEvent, res);
+
+});
+
+module.exports = router;
