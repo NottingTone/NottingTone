0e1bcd19698d78ad76e702895f1ac70e16a2cf27
SEIAROTg <seiarotg@gmail.com>
2015-11-11 15:35:14 +0800

    add access token refresh

--- config.js
+++ config.js
@@ -1,7 +1,22 @@
 var path     = require('path');
 var fs       = require('fs');
+var request  = require('request');
 
 // 读取API配置文件
 var config = JSON.parse(fs.readFileSync(path.join(__dirname, './config.json')));
+
+var grantToken = function () {
+	request.get(
+		'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + config.wechat.appid + '&secret=' + config.wechat.appsecret,
+		function (err, resp, body) {
+			config.wechat.accessToken = JSON.parse(body).access_token;
+		}
+	);
+}
+
+// 每小时刷新token
+grantToken();
+setInterval(grantToken, 1 * 60 * 60 * 1000);
+
 
 module.exports = config;
