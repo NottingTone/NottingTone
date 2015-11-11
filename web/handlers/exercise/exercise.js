dc2697254528d3275071a49f166fb20fbf05b6eb
Chunfan Gao <lycopene.work@gmail.com>
2015-11-11 01:28:59 +0000

    First try on exercise. It is still confused about the sendRawResponse function. Don't know whether it will automatically handle the timeout situation, or I need to write a if clause to judge whether it is timeout and send a null request to weixin server.

--- web/handlers/exercise/exercise.js
+++ web/handlers/exercise/exercise.js
@@ -1,11 +1,39 @@
 "use strict";
+var request = require('request');
 
-function exercise () {
+function exercise (req, res) {
+	var this_semester_pe_num_web = "";
+	var last_semester_pe_num_web = "";
+	
 	if (!this.user.info.stuId) {
 		this.handOver('REQUIRE_STUID');
 	} else {
 		// do stuff
-		this.sendTemplateResponse('unsupported');
+		var url_this_semester_pe_num = "http://unnctimetable.com/pe.php?stuid=" + this.user.info.stuId;
+		request(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				this_semester_pe_num_web += body;
+			} else {
+					// not success do stuff
+				this_semester_pe_num_web = "Oops, there are some problems happens about your pe information of this semester";
+			}
+
+			
+		})
+		var url_last_semester_pe_num = "http://unnctimetable.com/tiyuka.php?stuid=" + this.user.info.stuId;
+				request(url, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				last_semester_pe_num_web += body;
+			} else {
+					// not success do stuff
+					this_semester_pe_num_web = "Oops, there are some problems happens about your pe information of last semester";
+			}
+		})
+		
+		var outputString = this_semester_pe_num_web + "\n" + last_semester_pe_num_web;
+		
+		// send outputString to user
+		this.sendRawResponse('txt',outputString]);
 	}
 }
 
