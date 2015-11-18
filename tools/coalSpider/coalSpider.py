6a309377ce012220e5df3b0ff7f79972ed03e422
SEIAROTg <seiarotg@gmail.com>
2015-11-18 18:48:58 +0800

    change coalSpider

--- tools/coalSpider/coalSpider.py
+++ tools/coalSpider/coalSpider.py
@@ -16,118 +16,198 @@ import datetime, time
 import urlparse
 import urllib3
 import sqlite3
+import leveldb
+import json
+
+
+count = 0
 
 def str2fen(s):
 	return int(float(s)*100)
 
+def loadConfig():
+	global config
+	with open(os.path.join(os.path.dirname(__file__), '../../config.json')) as f:
+		config = json.load(f);
 
-if __name__ == '__main__':
-
-	uri_parsed = urlparse.urlparse(URL_QUERY)
-
-	http_pool = urllib3.HTTPConnectionPool(uri_parsed.hostname, uri_parsed.port)
-
-	conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), '../../db/coal.sqlite3'))
+def initDatabase():
+	global conn, cursor, saveCursor, roomCursor
+	conn = sqlite3.connect(os.path.join(os.path.dirname(__file__), '../..', config['db']['path'], config['db']['sqlite']['coal']))
 	cursor = conn.cursor()
-	room_list = conn.cursor()
-
+	saveCursor = conn.cursor()
+	roomCursor = conn.cursor()
 	cursor.execute('CREATE TABLE IF NOT EXISTS roomlist (id INTEGER PRIMARY KEY, build INTEGER, room INTEGER)')
 	cursor.execute('CREATE TABLE IF NOT EXISTS data (rid, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money, time)')
 	cursor.execute('CREATE TABLE IF NOT EXISTS usage (rid, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money, time)')
 
-	count = 0
-	room_list.execute('SELECT * FROM roomlist')
+	global userbind
+	userbind = leveldb.LevelDB(os.path.join(os.path.dirname(__file__), '../..', config['db']['path'], config['db']['level']['userbind']))
 
-	room = room_list.fetchone()
+def rowInserted():
+	global count
+	count += 1
+	if count % 100 == 0:
+		conn.commit()
 
+def updateRoomList():
+	for key, value in userbind.RangeIter(include_value = True):
+		data = json.loads(value)
+		if data.has_key('build') and data.has_key('room'):
+			if not checkRoomExist(data['build'], data['room']): # 假定列表里的房间已查询
+				ret = performQuery(data['build'], data['room'])
+				if ret:
+					print '新加一条'
+					roomCursor.execute('INSERT INTO roomlist (build, room) VALUES (?,?)', (data['build'], data['room']))
+					conn.commit()
+					rid = roomCursor.lastrowid
+					print rid, '<--'
+					saveResultByRid(rid, ret)
+
+def loadHTTPPool():
+	global uri_parsed, http_pool
+	uri_parsed = urlparse.urlparse(URL_QUERY)
+	http_pool = urllib3.HTTPConnectionPool(uri_parsed.hostname, uri_parsed.port)
+
+def performQueryForRoomList():
+	roomCursor.execute('SELECT id, build, room FROM roomlist')
+	room = roomCursor.fetchone()
 	while room:
-
-		rid = room['id']
-		build = room['build']
-		room = room['room']
-
-		data = "build={0}&room={1}&xw=%D0%A3%CD%E2%B2%E9%D1%AF".format(build, room)
-		req = http_pool.urlopen('POST', uri_parsed.path, headers={'Content-Type': 'application/x-www-form-urlencoded'}, body=data)
-		html = req.data.decode('gbk')
-
-		if re.search(REGEX_BACK, html):
-			print 'BUILD {0} ROOM {1} DOES NOT EXIST'.format(build, room)
+		print room
+		if not checkTodayExistByRid(room[0]):
+			print '还没数据'
+			ret = performQuery(room[1], room[2])
+			saveResultByRid(room[0], ret)
 		else:
-			time_str = re.search(REGEX_TIME, html).group(1)
-			time_datetime = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M %p')
-			timestamp = int(time.mktime(time_datetime.timetuple()))
+			print '已经有了'
+		room = roomCursor.fetchone()
 
-			tr_tags = re.findall(REGEX_TR, html)
+def performQuery(build, room):
+	print 'query', build, room
+	data = "build={0}&room={1}&xw=%D0%A3%CD%E2%B2%E9%D1%AF".format(build, room)
+	req = http_pool.urlopen('POST', uri_parsed.path, headers={'Content-Type': 'application/x-www-form-urlencoded'}, body=data)
+	html = req.data.decode('gbk')
 
-			ret = {}
+	if re.search(REGEX_BACK, html):
+		return None
+	else:
 
-			for tr_tag in tr_tags:
-				if re.search(REGEX_TH, tr_tag):
-					continue
-				td_tags = re.findall(REGEX_TD, tr_tag)
+		ret = {}
 
-				value_match = re.search(REGEX_NUM, td_tags[1])
-				if value_match:
-					value = str2fen(value_match.group(0))
-				else:
-					value = -1
+		time_str = re.search(REGEX_TIME, html).group(1)
+		time_datetime = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M %p')
+		ret['timestamp'] = int(time.mktime(time_datetime.timetuple()))
 
-				money_match = re.search(REGEX_NUM, td_tags[2])
-				if money_match:
-					money = str2fen(money_match.group(0))
-				else:
-					money = -1
+		tr_tags = re.findall(REGEX_TR, html)
 
-				item = {
-					'value': value,
-					'money': money
-				}
+		for tr_tag in tr_tags:
+			if re.search(REGEX_TH, tr_tag):
+				continue
+			td_tags = re.findall(REGEX_TD, tr_tag)
 
-				if td_tags[0].find(u'冷水表') != -1:
-					ret['water'] = item
-				if td_tags[0].find(u'热水表') != -1:
-					ret['hotwater'] = item
-				if td_tags[0].find(u'电表') != -1:
-					ret['electricity'] = item
-
-			cursor.execute('SELECT * FROM data WHERE rid=? AND time=? LIMIT 1', (rid, timestamp))
-			time_str = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-			if cursor.fetchone():
-				print 'ENTRY BUILD {0} ROOM {1} TIME {2} ALREADY EXISTS.'.format(build, room, time_str)
+			value_match = re.search(REGEX_NUM, td_tags[1])
+			if value_match:
+				value = str2fen(value_match.group(0))
 			else:
-				cursor.execute('SELECT * FROM data WHERE rid=? AND time>=?-86400 AND time<? LIMIT 1', (timestamp,))
-				yesterday = cursor.fetchone()
-				if yesterday:
-					cursor.execute('INSERT INTO usage (rid, time, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money) VALUES (?,?,?,?,?,?,?,?)',
-						(rid, timestamp,
-							ret['water']['value'] - yesterday['water_value'],
-							ret['water']['money'] - yesterday['water_money'],
-							ret['hotwater']['value'] - yesterday['hotwater_money'],
-							ret['hotwater']['money'] - yesterday['hotwater_money'],
-							ret['electricity']['value'] - yesterday['electricity_value'],
-							ret['electricity']['money'] - yesterday['electricity_money']
-						)
-					)
-				
-				cursor.execute('INSERT INTO data (rid, time, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money) VALUES (?,?,?,?,?,?,?,?)',
-					(rid, timestamp, 
-						ret['water']['value'], 
-						ret['water']['money'], 
-						ret['hotwater']['value'], 
-						ret['hotwater']['money'], 
-						ret['electricity']['value'], 
-						ret['electricity']['money']
-					)
-				)
-				count += 1
-				print 'BUILD {0} ROOM {1} TIME {2} DONE.'.format(build, room, time_str)
+				value = -1
 
-			if count % 100 == 0:
-				conn.commit()
+			money_match = re.search(REGEX_NUM, td_tags[2])
+			if money_match:
+				money = str2fen(money_match.group(0))
+			else:
+				money = -1
 
-		room = room_list.fetchone()
+			item = {
+				'value': value,
+				'money': money
+			}
 
+			if td_tags[0].find(u'冷水表') != -1:
+				ret['water'] = item
+			if td_tags[0].find(u'热水表') != -1:
+				ret['hotwater'] = item
+			if td_tags[0].find(u'电表') != -1:
+				ret['electricity'] = item
+
+		return ret
+
+
+def checkRoomExist(build, room):
+	cursor.execute('SELECT id FROM roomlist WHERE build=? AND room=? LIMIT 1', (build, room))
+	room = cursor.fetchone()
+	if room:
+		return room[0]
+	else:
+		return False
+
+def checkTodayExistByRid(rid):
+	now = int(time.time())
+	cursor.execute('SELECT * FROM data WHERE rid=? AND time>=?-86400 AND time<? LIMIT 1', (rid, now, now))
+	if (cursor.fetchone()):
+		return True
+	else:
+		return False
+
+def checkExistByRoom(build, room):
+	rid = checkRoomExist
+	if rid:
+		return checkTodayExistByRid(room['rid'])
+	else:
+		return False
+
+def getYesterdayByRid(rid):
+	now = int(time.time())
+	cursor.execute('SELECT water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money FROM data WHERE rid=? AND time>=?-172800 AND time<?-86400 LIMIT 1', (rid, now, now))
+	yesterday = cursor.fetchone()
+	if yesterday:
+		return yesterday
+	else:
+		return None
+
+def getUsage(today, yesterday):
+	if today == -1 or yesterday == -1:
+		return -1
+	else:
+		return today - yesterday
+
+def saveResultByRid(rid, data):
+	saveCursor.execute('INSERT INTO data (rid, time, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money) VALUES (?,?,?,?,?,?,?,?)',
+		(rid, data['timestamp'], 
+			data['water']['value'], 
+			data['water']['money'], 
+			data['hotwater']['value'], 
+			data['hotwater']['money'], 
+			data['electricity']['value'], 
+			data['electricity']['money']
+		)
+	)
+	yesterday = getYesterdayByRid(rid)
+	if yesterday:
+		saveCursor.execute('INSERT INTO usage (rid, time, water_value, water_money, hotwater_value, hotwater_money, electricity_value, electricity_money) VALUES (?,?,?,?,?,?,?,?)',
+			(rid, data['timestamp'],
+				getUsage(data['water']['value'], yesterday[0]),
+				getUsage(data['water']['money'], yesterday[1]),
+				getUsage(data['hotwater']['value'], yesterday[2]),
+				getUsage(data['hotwater']['money'], yesterday[3]),
+				getUsage(data['electricity']['value'], yesterday[4]),
+				getUsage(data['electricity']['money'], yesterday[5])
+			)
+		)
+	rowInserted()
+
+def closeDatabase():
 	conn.commit()
 	conn.close()
+
+
+if __name__ == '__main__':
+
+	count = 0
+
+	loadConfig()
+	initDatabase()
+	loadHTTPPool()
+	performQueryForRoomList()
+	updateRoomList()
+	closeDatabase()
 
 	print 'ALL DONE. AFFECTED ROOMS: %d' % count
