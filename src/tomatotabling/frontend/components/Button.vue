4da54812f920d8beee85bac01be7338e1d91b3df
SEIAROTg <seiarotg@gmail.com>
2017-03-02 00:00:41 +0800

    implement tomatotabling frontend Button component

---
+++ src/tomatotabling/frontend/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+
+<cell :title="title" :link="link" class="tt-button" :class="{'vux-tap-active': true}"></cell>
+
+</template>
+
+<script>
+import { Cell } from 'vux';
+
+export default {
+	props: {
+		title: String,
+		link: [String, Object],
+	},
+	components: {
+		Cell,
+	},
+};
+</script>
+
+<style>
+.tt-button>div.weui_cell_primary>p {
+	color: #fe0047;
+}
+</style>
