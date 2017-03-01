a7a4928be65a81c9a3a97dce443a4a838c1b861e
SEIAROTg <seiarotg@gmail.com>
2017-03-01 23:51:01 +0800

    implement wechat exercise handler

---
+++ src/wechat/handlers/exercise/index.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+
+import { getOverview } from '../../../services/exercise';
+export config from './config';
+
+export default async function () {
+	const stuId = await this.callHandler('INPUT_STUID');
+	const overview = await getOverview(stuId);
+	let lastRecord;
+	if (overview.thisTerm.last === -1) {
+		lastRecord = 'None';
+	} else {
+		lastRecord = moment.unix(overview.thisTerm.last).locale(this.user.info.lang).format('ll HH:mm');
+	}
+	await this.sendTemplateResponse('exercise', {
+		lastTermNum: overview.lastTerm.number,
+		thisTermNum: overview.thisTerm.number,
+		lastRecord,
+	});
+	this.interrupt();
+}
