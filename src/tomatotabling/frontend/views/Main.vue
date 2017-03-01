ba563b9d685f269a0783409dd2a81052313d18bb
SEIAROTg <seiarotg@gmail.com>
2017-03-02 00:01:27 +0800

    implement tomatotabling frontend Main view

---
+++ src/tomatotabling/frontend/views/Main.vue
@@ -0,0 +1,123 @@
+<template>
+<div style="height: 100%">
+	<loading v-model="isLoadingFake" v-show="loading > 0" text="Loading"></loading>
+	<view-box :class="{ nopadding: !shownTabbar }">
+		<keep-alive>
+			<router-view @show-bars="showBars"></router-view>
+		</keep-alive>
+		<tabbar v-show="isMain && shownTabbar" slot="bottom">
+			<tabbar-item link="/timetable" :selected="!!$route.path.match(/^\/timetable(\/|$)/)" icon-class="tti tti-timetable">
+				<div slot="icon"></div>
+				<span slot="label">Timetable</span>
+			</tabbar-item>
+			<tabbar-item link="/modules" :selected="!!$route.path.match(/^\/modules(\/|$)/)" icon-class="tti tti-module">
+				<div slot="icon"></div>
+				<span slot="label">Modules</span>
+			</tabbar-item>
+			<tabbar-item link="/more" :selected="!!$route.path.match(/^\/more(\/|$)/)" icon-class="tti tti-more">
+				<div slot="icon"></div>
+				<span slot="label">More</span>
+			</tabbar-item>
+		</tabbar>
+	</view-box>
+	<div class="export" v-show="$route.query.export && loading === 0" @click="$router.replace({})">
+		<div class="text">Click the menu and open in browser</div>
+	</div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue';
+import { Tabbar, TabbarItem, ViewBox, Loading, AlertPlugin, ConfirmPlugin } from 'vux';
+
+Vue.use(AlertPlugin);
+Vue.use(ConfirmPlugin);
+
+export default {
+	components: {
+		Tabbar,
+		TabbarItem,
+		ViewBox,
+		Loading,
+	},
+	computed: {
+		isMain() {
+			return /^\/(modules$|more$|timetable)/.test(this.$route.path);
+		},
+	},
+	data() {
+		return {
+			isLoadingFake: true,
+			loading: 0,
+			shownTabbar: true,
+		};
+	},
+	created() {
+		Vue.http.interceptors.push((req, next) => {
+			// defer loading, preventing flashing loading
+			setTimeout(() => ++this.loading, 100);
+			next(() => {
+				--this.loading;
+			});
+		});
+	},
+	methods: {
+		showBars(shown) {
+			this.shownTabbar = shown;
+		},
+	},
+};
+</script>
+
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+</style>
+
+<style>
+body {
+	background: #fbf9fe;
+	user-select: none;
+}
+.weui_tabbar_item>.weui_tabbar_icon {
+	font-size: 24px;
+	color: #888;
+}
+.weui_tabbar_item.weui_bar_item_on>.weui_tabbar_icon {
+	color: #09BB07;
+}
+html, body {
+	height: 100%;
+	width: 100%;
+	overflow-x: hidden;
+}
+input, textarea {
+	user-select: text;
+}
+i.tti {
+	display: inline-block;
+	vertical-align: middle;
+}
+.nopadding>#vux_view_box_body {
+	padding: 0;
+}
+</style>
+
+<style scoped>
+.export {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: url(../imgs/wx.png) top 20px right 20px no-repeat #000;
+	opacity: .6;
+	color: #fff;
+	z-index: 101; /* tabbar = 100 */
+}
+.export > .text {
+	position: absolute;
+	top: 50px;
+	right: 80px;
+}
+</style>
