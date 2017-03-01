6f1a280e7eeb83f956915846637f77462b1737f3
SEIAROTg <seiarotg@gmail.com>
2017-03-01 23:59:25 +0800

    implement tomatotabling frontend entry

---
+++ src/tomatotabling/frontend/main.js
@@ -0,0 +1,52 @@
+import 'babel-polyfill';
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import FastClick from 'fastclick';
+
+import './icons.font.json';
+import App from './views/Main';
+import router from './router';
+import data from './data';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+Vue.use(data);
+
+Vue.http.options.root = '/tomatotabling';
+Vue.http.interceptors.push(function (req, next) {
+	const timeout = setTimeout(() => {
+		next(req.respondWith(JSON.stringify({
+			code: 408,
+			message: 'Network Error',
+		}), {
+			status: 408,
+			statusText: 'Timeout',
+		}));
+	}, 5000);
+	next((resp) => {
+		clearTimeout(timeout);
+		if (resp.status === 401) {
+			window.location.reload();
+		} else if (resp.status >= 500) {
+			resp.message = 'Internal Error';
+		} else if (resp.status === 429) {
+			resp.message = 'Request Too Frequent';
+		} else {
+			try {
+				resp.data = JSON.parse(resp.body);
+			} catch (e) {
+				resp.message = 'Internal Error';
+			}
+		}
+	});
+});
+
+FastClick.attach(document.body);
+
+new Vue({
+	router,
+	render: h => h(App),
+}).$mount('app');
+
