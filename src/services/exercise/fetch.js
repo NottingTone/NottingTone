914af3635c02f44d4c892a95d805b46edfd523ce
SEIAROTg <seiarotg@gmail.com>
2017-03-23 21:50:00 +0800

    fix exercise count

--- src/services/exercise/fetch.js
+++ src/services/exercise/fetch.js
@@ -24,12 +24,19 @@ async function fetchRecords(stuId, term, type) {
 		},
 	});
 	const html = gbk2utf8.convert(body).toString();
-	const pattern = /<td width=177 bgcolor=#ffffff height=25>([\s\S]*?)&nbsp;<\/td>/g;
-	let match;
+	const pattern = /<tr align=middle bgcolor=#efefef>([\s\S]*?)<\/tr>/g;
+	let match = pattern.exec(html); // skip first match (table header)
 	const times = [];
 	while ((match = pattern.exec(html))) {
-		const time = moment(match[1], 'M/D/YYYY H:m:s A').unix();
-		times.push(time); 
+		const tds = match[1].split(/\s*<.*?>\s*(?:<.*?>)?/).slice(1, -1).map(x => x.replace(/&nbsp;/g, ''));
+		if (tds.length === 6) {
+			const number = parseInt(tds[3], 10);
+			const time = moment(tds[5], 'M/D/YYYY H:m:s A').unix();
+			times.push({
+				number,
+				time,
+			}); 
+		}
 	}
 	return times;
 }
@@ -39,8 +46,8 @@ export async function fetchOverview(stuId) {
 	for (const term of ['lastTerm', 'thisTerm']) {
 		const records = await fetchRecords(stuId, term, 'run');
 		overview[term] = {
-			number: records.length,
-			last: records.length ? records.slice(-1)[0] : -1,
+			number: records.reduce((a, b) => a + b.number, 0),
+			last: records.length ? records.slice(-1)[0].time : -1,
 		};
 	}
 	return overview;
