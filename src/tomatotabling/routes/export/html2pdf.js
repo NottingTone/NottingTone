5cd5e587a29d9a4f57b6959ff58e6dbfe6967cc9
SEIAROTg <seiarotg@gmail.com>
2017-09-18 14:58:41 +0800

    switch to puppeteer

--- src/tomatotabling/routes/export/html2pdf.js
+++ src/tomatotabling/routes/export/html2pdf.js
@@ -1,17 +1,20 @@
-import pdf from 'html-pdf';
+import puppeteer from 'puppeteer';
 
-export default function html2pdf(html) {
-	return new Promise((resolve, reject) => {
-		pdf.create(html, {
-			height: '840px',
-			width: '1188px',
-			border: 0,
-		}).toStream((err, stream) => {
-			if (err) {
-				reject(err);
-			} else {
-				resolve(stream);
-			}
-		});
+let browser;
+
+puppeteer.launch()
+	.then(b => browser = b);
+
+export default async function html2pdf(html) {
+	const page = await browser.newPage();
+	await page.setContent(html);
+	await page.emulateMedia('screen');
+	const pdf = await page.pdf({
+		landscape: true,
+		height: '1188px',
+		width: '840px',
+		printBackground: true,
 	});
+	await page.close();
+	return pdf;
 };
