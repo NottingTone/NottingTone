d42126d96521b5adb9d309040cb58f97da778b1a
SEIAROTg <seiarotg@gmail.com>
2017-09-20 16:33:44 +0800

    fix name input bug

--- src/wechat/handlers/input/name.js
+++ src/wechat/handlers/input/name.js
@@ -2,7 +2,7 @@ export default async function () {
 	if (this.isRestoredHandler()) {
 		this.log.func = 'input/name';
 		const input = this.takeTextInput();
-		if (input && input.match(/^[A-Za-z '\u4e00-\u9fa5]{2,30}$/)) {
+		if (input && input.match(/^[A-Za-z '-\u4e00-\u9fa5]{2,30}$/)) {
 			return input.trim();
 		} else {
 			await this.sendTemplateResponse('input/invalid');
